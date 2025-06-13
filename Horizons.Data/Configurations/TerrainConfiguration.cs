22a65b5b663a5bf647e836522d97470e745465ac
natrapNiko <natrapniko@gmail.com>
2025-06-13 18:10:41 +0200

    Refactored seed data into separate configuration classes!
    - Created a new IdentityUserConfiguration class in Horizons.Data/Configuration for seeding IdentityUser.
    - Did the same for Terrain seed data and moved it to TerrainConfiguration.
    - Also moved Destination seed data to DestinationConfiguration.
    
    Additionally, added DbSet properties for Destination, Terrain, and UserDestination in HorizonDbContext.cs.

--- Horizons.Data/Configurations/TerrainConfiguration.cs
+++ Horizons.Data/Configurations/TerrainConfiguration.cs
@@ -19,6 +19,26 @@ namespace Horizons.Data.Configurations
                 .Property(t => t.Name)
                 .IsRequired()
                 .HasMaxLength(TerrainMaxLength);
+
+            //entity
+            //    .HasData(this.GenerateSeedTerrains());
+        }
+
+        private List<Terrain> GenerateSeedTerrains()
+        {
+            List<Terrain> terrains = new List<Terrain>()
+            {
+                new Terrain { Id = 1, Name = "Mountain" },
+                new Terrain { Id = 2, Name = "Beach" },
+                new Terrain { Id = 3, Name = "Forest" },
+                new Terrain { Id = 4, Name = "Plain" },
+                new Terrain { Id = 5, Name = "Urban" },
+                new Terrain { Id = 6, Name = "Village" },
+                new Terrain { Id = 7, Name = "Cave" },
+                new Terrain { Id = 8, Name = "Canyon" }
+            };
+
+            return terrains;
         }
     }
 }
---
+++ Horizons.Data/HorizonDbContext.cs
@@ -0,0 +1,31 @@
+﻿namespace Horizons.Data
+{
+    using Horizons.Data.Models;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    using System.Reflection;
+
+    public class HorizonDbContext : IdentityDbContext
+    {
+        public HorizonDbContext(DbContextOptions<HorizonDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Destination> Destinations { get; set; } = null!;
+
+        public virtual DbSet<Terrain> Terrains { get; set; } = null!;
+
+        public virtual DbSet<UserDestination> UsersDestinations { get; set; } = null!;
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
